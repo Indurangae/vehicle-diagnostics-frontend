360bb0f4563963f54b40780f11fb0776051415b2
Induranga Ekanayake <125475051+Indurangae@users.noreply.github.com>
2025-05-07 19:29:37 +0100

    Update Register.jsx

--- src/components/Sign/Register.jsx
+++ src/components/Sign/Register.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "./Register.css"; // Scoped CSS import
+import CONFIG from '../../config';
 
 const Register = () => {
   const [name, setName] = useState("");
