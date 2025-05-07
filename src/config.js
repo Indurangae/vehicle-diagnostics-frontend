7818453d2ffe1a29757fcb658f12f3d3902bdf16
Induranga Ekanayake <125475051+Indurangae@users.noreply.github.com>
2025-05-07 19:18:49 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    API_BASE_URL: "https://vehicle-diagnostics-frontend.onrender.com",
+    API_BASE_URL: "https://node-backend-7jky.onrender.com",
     ML_API_URL: "https://ml-maintenance-api.onrender.com"
   };
   
